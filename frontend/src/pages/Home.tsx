ea296e509a459578e9ee8b60956868248f7bd7a4
Swastika Yadav <44317853+swastika0015@users.noreply.github.com>
2024-08-05 18:42:39 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,9 +2,6 @@
 
 import React from 'react';
 import LinkForm from "../components/LinkForm";
-import { CopilotKit } from "@copilotkit/react-core";
-import { CopilotSidebar } from "@copilotkit/react-ui";
-import "@copilotkit/react-ui/styles.css";
 
 
 const Home: React.FC = () => {
@@ -12,10 +9,6 @@ const Home: React.FC = () => {
     <div className="min-h-screen flex flex-col items-center justify-center  p-4 bg-gradient-to-r from-indigo-200 to-purple-100">
       <h1 className="text-3xl font-extrabold m-4 mt-4 text-indigo-700">LinkNest</h1>
       <LinkForm />
-      <CopilotKit publicApiKey="the api key or self host (see below)">
-      <CopilotSidebar>
-      </CopilotSidebar>
-    </CopilotKit>
     </div>
   );
 };
